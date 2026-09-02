435bd66ad10080ba1ca632866ab22ce5a65f47e3
Uman-8679 <uammu.32@gmail.com>
2026-09-02 19:59:17 +0530

    Add files via upload
    Day2

---
+++ day2/javascriptassignment.js
@@ -0,0 +1,11 @@
+var firstname = "uma";
+var companyname = "wipro";
+var mobilenumber = 8095860319;
+var isAutomation = true;
+var hasPlaywright;
+
+console.log(firstname, typeof firstname);
+console.log(companyname, typeof companyname);
+console.log(mobilenumber, typeof mobilenumber);
+console.log(isAutomation, typeof isAutomation);
+console.log(hasPlaywright, typeof hasPlaywright);
