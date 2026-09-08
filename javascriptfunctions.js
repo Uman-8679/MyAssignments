73cea160e7f3bb67dab939dbb7e92a2fa88f4048
Uman-8679 <uammu.32@gmail.com>
2026-09-08 10:40:15 +0530

    Add files via upload
    day5

---
+++ javascriptfunctions.js
@@ -0,0 +1,36 @@
+// Task 1: Function Declaration
+function userProfile(name) 
+{
+    console.log("Hello, " + name + "!");
+}
+userProfile("Uma");
+
+// Task 2: Arrow Function
+const double = (number) => 
+{
+    return number * 2;
+};
+
+console.log(double(5));
+
+// Task 3: Anonymous Function
+setTimeout(function () 
+{
+    console.log("This message is delayed by 2 seconds");
+}, 2000);
+
+
+// Task 4: Callback Function
+function getUserData(callback)
+{
+    setTimeout(function () 
+    {
+        callback();
+    }, 3000);
+}
+
+// Calling getUserData with a callback function
+getUserData(function () 
+{
+    console.log("Call Back Function");
+});
