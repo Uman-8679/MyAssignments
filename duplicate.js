0a78b950ff6150812d32c139076381c163db14a3
Uman-8679 <uammu.32@gmail.com>
2026-09-08 10:55:16 +0530

    Delete duplicate.js

--- duplicate.js
+++
@@ -1,9 +0,0 @@
-let num = [56, 78, 90, 23, 90, 76, 43, 56];
-
-for (let i = 0; i < num.length; i++) {
-    for (let j = i + 1; j < num.length; j++) {
-        if (num[i] === num[j]) {
-            console.log(num[i] + " is duplicate");
-        }
-    }
-}
